7e7f3a76b1e3ca2dbdffbc34b4405f42dea9f02b
NaingMinThant77 <naingminthant07@gmail.com>
2025-03-23 21:14:59 +0630

    CounterApp

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { useCounterStore } from './store'
+
+function App() {
+  const count = useCounterStore((state) => state.count)
+  const increment = useCounterStore((state) => state.increment)
+  const decrement = useCounterStore((state) => state.decrement)
+  const reset = useCounterStore((state) => state.reset)
+
+  return (
+    <div className="counter-container">
+      <h2>Counter App with Zustand</h2>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default App
